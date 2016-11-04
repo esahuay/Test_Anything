1a2feba7a731b50b77a41267c24b6746788db2bc
esahuay <eliseosahuay@gmail.com>
2016-11-04 14:06:39 -0600

    agregando test_anything

--- archivo5.js
+++ archivo5.js
@@ -4,6 +4,7 @@ var test = require('tape')
 test('aceptado',function(t){
 	t.plan(3);
 	t.equal(alimenta('comida'),'yum');
-	t.equal(alimenta('pez'),'yum')
- t.throws(alimenta.bind(null,'chocolate'));
+	t.equal(alimenta('pez'),'yum');
+	t.throws(alimenta.bind(null,'chocolate'));
+	t.end()
 });
